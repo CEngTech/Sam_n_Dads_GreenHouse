84d01ff038c8be80528a818404090a06316fc9c2
DSHardy <darren_hardy@icloud.com>
2020-04-12 13:58:43 +0100

    Edits to 'Sam_n_Dads_Greenhouse.ino' - Added funtionality for humiture sensor Signed-off-by: DSHardy <darren_hardy@icloud.com>

--- Sam_n_Dads_GreenHouse/Sam_n_Dads_GreenHouse.ino
+++ Sam_n_Dads_GreenHouse/Sam_n_Dads_GreenHouse.ino
@@ -1,34 +1,42 @@
 
-
+//include the libraries
 #include <dht.h>
 #include <Wire.h> 
-// set the LCD address to 0x27 for a 16 chars and 2 line display
+
 dht DHT;//create a variable type of dht
-const int DHT11_PIN= 7;//Humiture sensor attach to pin7
+
+const int DHT11_PIN= 7;//Humiture sensor attach to Digital pin7
+
 void setup()
 {
-  Serial.begin(9600);//initialize the serial
+  Serial.begin(115200);//initialize the serial
 }
+
 void loop()
 {
   // READ DATA
   //Serial.println("DHT11:");
-
+  
   int chk = DHT.read11(DHT11_PIN);//read the value returned from sensor
   switch (chk)
   {
-    case DHTLIB_OK: 
-    //Serial.println("OK!"); 
+  case DHTLIB_OK:  
+    Serial.println(" OK!"); 
     break;
-    case DHTLIB_ERROR_CHECKSUM: 
-    //Serial.print("Checksum error,\t"); 
+  case DHTLIB_ERROR_CHECKSUM: 
+    Serial.print("Checksum error,\t"); 
     break;
-    case DHTLIB_ERROR_TIMEOUT: 
-    //Serial.print("Time out error,\t"); 
+  case DHTLIB_ERROR_TIMEOUT: 
+    Serial.print("Time out error,\t"); 
     break;
-    default: 
-    //Serial.print("Unknown error,\t"); 
+  default: 
+    Serial.print("Unknown error,\t"); 
     break;
   }
- 
+  delay(4000); //wait a while 
+  Serial.print("Tem: ");
+  Serial.print(DHT.temperature,1);
+  Serial.print("  ");
+  Serial.print("Hum: ");
+  Serial.print(DHT.humidity,1);
 }
