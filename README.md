14b0cfdf846408729da60ade5000e4cf5ad79adc
SSHardy <sammyshardy@icloud.com>
2020-04-12 18:21:40 +0100

    Edits to 'README.md' - Pin Constant names added
    Edits to 'Sam_n_Dads_GreenHouse.ino' - SMS Functionality updated and Pin constants added
    
    Signed-off-by: SSHardy <sammyshardy@icloud.com>

--- README.md
+++ README.md
@@ -8,11 +8,26 @@ Pin allocations for the Arduino:
 	- Soil 2 input (analogue pin) A1
 	- Soil 3 input (analogue pin) A2
 	- Soil 4 input (analogue pin) A3
-	- photoresistor (analogue pin) A4
+	- PhotoResistor (analogue pin) A4
 
-	- motor 1 (small) output (digital pin) D2 
-	- motor 2 (large) output (digital pin) D3
+	- Motor 1 (small) output (digital pin) D2 
+	- Motor 2 (large) output (digital pin) D3
 	- Humiture detector (digital pin) D4
 
+Pin Constant Names:
+
+	Digital Pins - 
+		DM1_PIN - Motor 1
+		DM2_PIN - Motor 2
+		DHT11_PIN - Humiture Sensor
+
+	Analog Pins - 
+		AS1_PIN - Soil 1 input 
+		AS2_PIN - Soil 2 input 
+		AS3_PIN - Soil 3 input 
+		AS4_PIN - Soil 4 input 
+		APR_PIN - PhotoResistor
+	
+
 Resources:
-	SMS on Arduino with GSM Shield - https://www.arduino.cc/en/Tutorial/GSMExamplesSendSMS
+	SMS on Arduino with GSM - https://www.arduino.cc/en/Tutorial/GSMExamplesSendSMS
--- Sam_n_Dads_GreenHouse/Sam_n_Dads_GreenHouse.ino
+++ Sam_n_Dads_GreenHouse/Sam_n_Dads_GreenHouse.ino
@@ -12,40 +12,87 @@ GSM_SMS sms;
 //set the LCD address to 0x27 for a 16 chars and 2 line display
 dht DHT;//create a variable type of dht
 
-const int DHT11_PIN= 7;//Humiture sensor attach to pin7
+//Function Decleration
+void humitureCheck();
+
+//Digital Pins
+const int DM1_PIN = 2; //Motor 1 attach to pin2
+const int DM2_PIN = 3; //Motor 1 attach to pin3
+const int DHT11_PIN = 4; //Humiture sensor attach to pin4
+//Analog Pins
+const int AS1_PIN = 0; //Soil 1 sensor attach to pin0
+const int AS2_PIN = 1; //Soil 2 sensor attach to pin1
+const int AS3_PIN = 2; //Soil 3 sensor attach to pin2
+const int AS4_PIN = 3; //Soil 4 sensor attach to pin3
+const int APR_PIN = 4; //PhotoResitor sensor attach to pin4
+  
 
 void setup() {
-  Serial.begin(9600);//initialize the serials
-
-  boolean notConnected = true;
+  Serial.begin(115200);//initialize the serials
 
   //Check for SMS network connection
+  boolean notConnected = true;
   while(notConnected) {
     if(gsmAccess.begin(PINNUMBER)==GSM_READY)
       //connected
       notConnected = false;
     else {
       //not connected
-      Serial.println("Not connected");
+      //Serial.println("Not connected");
       delay(1000);
     }
   }
-  
-  Serial.println("GSM initialized."); 
+  //Serial.println("GSM initialized."); 
 }
 
 void loop() {
   //READ DATA
   //Serial.println("DHT11:");
+  //SMS
   char txtMsg; //Text Message to be sent
-  char remoteNumber1 = "07773571078"; //First number to send SMS too
-  char remoteNumber2 = "07460780161"; //Second number to send SMS too
+  char remoteNumber [] = { "07773571078", "07460780161" } ; //Numbers to send SMS to
+  int remoteNumLen = sizeof(remoteNumber) / sizeof(char); //Get length of remoteNumber array 
+  //Humiture
+  int greenhouseTemp = DHT.temperature; //Temperature of greenhouse
+  //int minTemp = ;
+  //int maxTemp = ;
+  int greenhouseHum = DHT.humidity ; //Humidity of greenhouse
+  //int minHum = ;
+  //int maxHum = ;
+
+  if (greenhouseTemp > maxTemp) {
+    char txtMsg = "Greenhouse Too Hot!";
+    sendSMS(remoteNumber, remoteNumLen, txtMsg);
+  } else if (greenhouseTemp < minTemp) {
+    char txtMsg = "Greenhouse Too Cold!";
+    sendSMS(remoteNumber, remoteNumLen, txtMsg);
+  }
+
+  if (greenhouseHum > maxHum) {
+    char txtMsg = "Greenhouse Too Humid!";
+    sendSMS(remoteNumber, remoteNumLen, txtMsg);
+  } else if (greenhouseHum < minHum) {
+    char txtMsg = "Greenhouse Not Humid Enough!";
+    sendSMS(remoteNumber, remoteNumLen, txtMsg);
+  }
   
-  int chk = DHT.read11(DHT11_PIN);//read the value returned from sensor
-  switch (chk)
-  {
+}
+
+void sendSMS(remoteNumber, remoteNumLen, txtMsg) {
+  for (int i = 0; i < remoteNumLen; i = i + 1 ) {
+    sms.beginSMS(remoteNumber[i]);
+    sms.print(txtMsg);
+    sms.endSMS(); 
+  }
+}
+
+void humitureCheck() {
+  int chk = DHT.read11(DHT11_PIN);//read the value returned from Humiture sensor
+
+  //Humiture sensor check
+  switch (chk) {
     case DHTLIB_OK: 
-    //Serial.println("OK!"); 
+    //Serial.println("OK!");
     break;
     case DHTLIB_ERROR_CHECKSUM: 
     //Serial.print("Checksum error,\t"); 
